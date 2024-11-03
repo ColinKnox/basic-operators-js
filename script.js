a8855c54a565a68d84f52e3e4f492c06d40bd7d2
ColinKnox <colinwknox.com>
2024-11-02 21:12:34 -0400

    Adding starter code.

---
+++ script.js
@@ -0,0 +1,19 @@
+let population = 667120;
+const finlandPopulation = 6000000;
+const averagePopulation = 33000000;
+const city = "Boston";
+const district = "7th District";
+const usStateRepresentative = "Ayanna Pressley";
+const language = "English";
+
+console.log(population / 2);
+
+population++;
+
+console.log(population);
+console.log(population > finlandPopulation);
+console.log(population > averagePopulation);
+
+const description = `${city} is a city in the ${district}. It's representative is ${usStateRepresentative}. It has a population of ${population} people that speak ${language}.`;
+
+console.log(description);
